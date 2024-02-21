8e627434e389bf495346decb2aa8a8948b4dbfa8
valleXYZ <valentin.seehausen@gmail.com>
2024-02-21 16:13:13 +0100

    add to eslintrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,15 +5,15 @@ module.exports = {
   },
   extends: [
     "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:@typescript-eslint/stylistic-type-checked",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
     project: true,
-    tsconfigRootDir: __dirname
+    tsconfigRootDir: __dirname,
   },
   plugins: ["@typescript-eslint"],
   rules: {},
