4d7a04e4cd0511c1309dbf05e17ebea4c2bfc822
valleXYZ <valentin.seehausen@gmail.com>
2024-03-01 22:06:43 +0100

    add close event

--- test/test.ts
+++ test/test.ts
@@ -13,8 +13,21 @@ describe("Transfers", () => {
 			id: "123",
 			owner: userAddress1,
 			collateral: 100_000n,
+			entryVolume: 500_000n,
+			assets: 500n,
+			entryPrice: 1_000n,
 			entryTimestamp: 2n,
+			borrowFeeIntegral: 100n,
+			fundingFeeIntegral: -200n,
 			direction: 1n,
+			isOpen: true,
+			closePrice: null,
+			closeTimestamp: null,
+			closeValue: null,
+			pnl: null,
+			borrowFeeAmount: null,
+			fundingFeeAmount: null,
+			totalPnL: null,
 		};
 
 		const mockPositionOpened = TradePair.PositionOpened.createMockEvent({
@@ -24,6 +37,9 @@ describe("Transfers", () => {
 			collateral: 100_000n,
 			volume: 500_000n,
 			direction: 1n,
+			assets: 500n,
+			borrowFeeIntegral: 100n,
+			fundingFeeIntegral: -200n,
 			mockEventData: {
 				blockNumber: 1,
 				blockTimestamp: 2,
@@ -48,5 +64,83 @@ describe("Transfers", () => {
 		assert.deepEqual(position1, mockPositionEntity, "Should be equal");
 		assert.equal(user1?.address, userAddress1, "user.address");
 		assert.equal(user1?.id, userAddress1, "user.id");
+	});
+
+	it("PositionClosed updates position", () => {
+		const mockDbEmpty = MockDb.createMockDb();
+
+		const userAddress1 = Addresses.mockAddresses[0];
+
+		const positionBefore: PositionEntity = {
+			id: "123",
+			owner: userAddress1,
+			collateral: 100_000n,
+			entryVolume: 500_000n,
+			assets: 500n,
+			entryPrice: 1_000n,
+			entryTimestamp: 2n,
+			borrowFeeIntegral: 100n,
+			fundingFeeIntegral: -200n,
+			direction: 1n,
+			isOpen: true,
+			closePrice: null,
+			closeTimestamp: null,
+			closeValue: null,
+			pnl: null,
+			borrowFeeAmount: null,
+			fundingFeeAmount: null,
+			totalPnL: null,
+		};
+
+		const updatedDb = mockDbEmpty.entities.Position.set(positionBefore);
+
+		const mockPositionClosed = TradePair.PositionClosed.createMockEvent({
+			owner: userAddress1,
+			id: 123n,
+			value: 590_000n,
+			closePrice: 1_200n,
+			borrowFeeAmount: 8_000n,
+			fundingFeeAmount: 2_000n,
+			mockEventData: {
+				blockNumber: 1,
+				blockTimestamp: 20,
+				blockHash: "0x0",
+				chainId: 1,
+				srcAddress: userAddress1,
+				transactionHash: "0x0",
+				transactionIndex: 1,
+				logIndex: 1,
+			},
+		});
+
+		const mockDbAfterPositionClosed = TradePair.PositionClosed.processEvent({
+			event: mockPositionClosed,
+			mockDb: updatedDb,
+		});
+
+		const position1 = mockDbAfterPositionClosed.entities.Position.get("123");
+
+		const expectedPosition: PositionEntity = {
+			id: "123",
+			owner: userAddress1,
+			collateral: 100_000n,
+			entryVolume: 500_000n,
+			assets: 500n,
+			entryPrice: 1_000n,
+			entryTimestamp: 2n,
+			borrowFeeIntegral: 100n,
+			fundingFeeIntegral: -200n,
+			direction: 1n,
+			isOpen: false,
+			closePrice: 1_200n,
+			closeTimestamp: 20n,
+			closeValue: 590_000n,
+			pnl: 100_000n,
+			borrowFeeAmount: 8_000n,
+			fundingFeeAmount: 2_000n,
+			totalPnL: 90_000n,
+		};
+
+		assert.deepEqual(position1, expectedPosition, "Should be equal");
 	});
 });
